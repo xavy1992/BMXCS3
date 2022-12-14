fe160ac363701dd797c2b8f75f5534b455dd5274
master <master@SMLAB408PC07_23>
2022-12-13 19:27:09 -0500

    Agregar archivos de proyecto.

---
+++ BMXCS3/Registro.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BMXCS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+        }
+    }
+}
---
+++ BMXCS3/login.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BMXCS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "BMX";
+            string contraseña = "123";
+            if(usuario == txtUsuario.Text && contraseña == txtContraseña.Text)
+            {
+                Navigation.PushAsync(new Registro(usuario));
+            }
+            else
+            {
+                DisplayAlert("ALERTA", "Usuario/Contraseña Incorrectos", "cerrar");
+            }
+         }
+    }
+}
